98a0f96aea43ef0701405cef79d1453a9f1bdab1
Laurent Haeffele <git@haeffele.com>
2017-03-01 17:38:07 +0100

    change kvm test

--- board/edufw/post-images.sh
+++ board/edufw/post-images.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+. "$BR2_EXTERNAL_EDUFW_PATH/rootfs-overlay/etc/edufw/version"
+touch "$BR2_EXTERNAL_EDUFW_PATH/iso/EDUFW_ISO-$EDUFW_VERSION"
+
 cp "$BINARIES_DIR/bzImage" "$BR2_EXTERNAL_EDUFW_PATH/iso/bzImage"
 cp "$BINARIES_DIR/rootfs.cpio.bz2" "$BR2_EXTERNAL_EDUFW_PATH/iso/initrd.bz2"
 "$HOST_DIR/usr/bin/grub-mkrescue" -o "$BINARIES_DIR/edufw.iso" --volid EDUFW_ISO "$BR2_EXTERNAL_EDUFW_PATH/iso/"
--- board/edufw/pre-iso.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-. $(dirname $0)/rootfs-overlay/etc/edufw/version
-touch "iso/EDUFW_ISO-$EDUFW_VERSION"
